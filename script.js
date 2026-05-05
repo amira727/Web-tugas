997d3ab382ae7e1430a4c5daeb0fce38e0fc471f
Amira <113337987+amira727@users.noreply.github.com>
2026-05-05 12:56:47 +0700

    script.js

--- script.js
+++ script.js
@@ -1,30 +1,23 @@
-// kunjungan
+// localstorage 
 let jumlahKunjungan = localStorage.getItem("kunjungan");
-
 if (jumlahKunjungan === null) {
     jumlahKunjungan = 1;
 } else {
     jumlahKunjungan = parseInt(jumlahKunjungan) + 1;
 }
-
 localStorage.setItem("kunjungan", jumlahKunjungan);
-
 document.getElementById("kunjungan").textContent =
     "Anda sudah mengunjungi profil ini " + jumlahKunjungan + " kali";
 
 // dark/light dan local
 const tombolMode = document.getElementById("toggleMode");
-
 const temaTersimpan = localStorage.getItem("tema");
-
 if (temaTersimpan === "dark") {
     document.body.classList.add("dark");
     tombolMode.textContent = "☀️ Light Mode";
 }
-
 tombolMode.addEventListener("click", function () {
     document.body.classList.toggle("dark");
-
     if (document.body.classList.contains("dark")) {
         localStorage.setItem("tema", "dark");
         tombolMode.textContent = "☀️ Light Mode";
@@ -34,7 +27,7 @@ tombolMode.addEventListener("click", function () {
     }
 });
 
-// SHOW HIDE HOBI
+// hobi
 const tombolHobi = document.getElementById("btnHobi");
 const dataHobi = document.getElementById("dataHobi");
 
@@ -48,30 +41,24 @@ tombolHobi.addEventListener("click", function () {
     }
 });
 
-// FORM VALIDATION
+// validasi
 const form = document.getElementById("contactForm");
-
 form.addEventListener("submit", function (e) {
     e.preventDefault();
-
     const nama = document.getElementById("nama").value;
     const email = document.getElementById("email").value;
     const pesan = document.getElementById("pesan").value;
-
     if (nama === "" || email === "" || pesan === "") {
         document.getElementById("hasil").textContent = "Semua field harus diisi!";
         document.getElementById("hasil").style.color = "red";
         return;
     }
-
     if (!email.includes("@")) {
         document.getElementById("hasil").textContent = "Email tidak valid!";
         document.getElementById("hasil").style.color = "red";
         return;
     }
-
     document.getElementById("hasil").textContent = "Pesan berhasil dikirim!";
     document.getElementById("hasil").style.color = "green";
-
     form.reset();
 });
